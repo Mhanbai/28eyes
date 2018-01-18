12af18edb5979b9e84801c4febce49e1532c396a
Mhanbai <gregormanby@gmail.com>
2018-01-18 00:49:52 +0000

    Added in some level editor tools - Button to move camera to correct rotation, Button to generate 'props' within a bounding box

---
+++ Assets/Editor/ReorientateCamera.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class ReorientateCamera : Editor {
+	[MenuItem("28Eyes Tools/1. Reorientate Camera")]
+	public static void MoveCamera() {
+		Transform bestView = GameObject.Find ("Main Camera").GetComponent<Camera> ().transform;
+		SceneView.lastActiveSceneView.AlignViewToObject (bestView);
+	}
+}

8d54f3ea786c41d0eee92572ec70e3363c338da8
BENJAMIN SAINT <1602915@uad.ac.uk>
2018-04-16 13:22:22 +0100

    balancing, animation bug, animation speed blah blah blah aren & ben.

--- Assets/Scripts/AttackList.cs
+++ Assets/Scripts/AttackList.cs
@@ -18,12 +18,12 @@ public class AttackList : MonoBehaviour {
 	public Attack[] attackType = new Attack[6];
 
 	void Start() {
-									// Name, Damage, Speed, Range, Movement, Number, Uses, Relaod Time, Prefab, Bleed, Poison, SLow
-		attackType [0] 	= new Attack ("Starting Pistol", 0.5f, 150.0f, 30.0f, 0, 1, 6, 1.0f, projectiles [0], false, false, false);
-		attackType [1] = new Attack ("Web", 5f, 25.0f, 15.0f, 1, 1, 8, 1.5f, projectiles [1], false, true, true);
-		attackType [2] = new Attack ("Claw", 6.0f, 10.0f, 20.0f, 0, 3, 6, 4.0f, projectiles [2], false, false, false);
-        attackType [3] = new Attack("S.Claw", 3.0f, 25.0f, 10.0f, 1, 4, 3, 2.0f, projectiles[0], false, true, true);
-        attackType [4] = new Attack("Wing", 1.5f, 25.0f, 10.0f, 1, 3, 2, 2.0f, projectiles[0], true, false, true);
-        attackType [5] = new Attack("Cat Claws", 4.5f, 10.0f, 10.0f, 1, 3, 1, 4.0f, projectiles[0], true, false, false);
+									// Name,            Damage, Speed,  Range, Movement, Number, Uses, Relaod Time, Prefab,             Bleed, Poison, SLow
+		attackType [0] = new Attack ("Starting Pistol", 0.5f,   150.0f, 30.0f,  0,          1,      6,  1.0f,       projectiles [0],    false, false, false);
+		attackType [1] = new Attack ("Web",             30.0f,   10.0f,  15.0f,  1,          1,      3,    2f,       projectiles [1],    false, true, true);
+		attackType [2] = new Attack ("Crab Claw",       50.0f,   5.0f,  50.0f,  0,          3,      1,    3f,       projectiles [2],    false, false, false);
+        attackType [3] = new Attack("S.Claw",           3.0f,   25.0f,  10.0f,  1,          4,      3,  2.0f,       projectiles[0],     false, true, true);
+        attackType [4] = new Attack("Wing",             1.5f,   25.0f,  10.0f,  1,          3,      2,  2.0f,       projectiles[0],     true, false, true);
+        attackType [5] = new Attack("Cat Claws",        4.5f,   10.0f,  10.0f,  1,          3,      1,  4.0f,       projectiles[0],     true, false, false);
     }
 }

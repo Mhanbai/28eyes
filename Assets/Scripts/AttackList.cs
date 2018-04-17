8e22c49a57ac48f92fc76b73ce9781da73653e77
BENJAMIN SAINT <1602915@uad.ac.uk>
2018-04-17 11:16:11 +0100

    Claw Explosion mainly

--- Assets/Scripts/AttackList.cs
+++ Assets/Scripts/AttackList.cs
@@ -15,13 +15,13 @@ public class AttackList : MonoBehaviour {
 	}
 
 	[SerializeField] GameObject[] projectiles;
-    [System.NonSerialized] public Attack[] attackType = new Attack[6];
+    [System.NonSerialized] public Attack[] attackType = new Attack[4];
 
 	void Start() {
 									// Name,            Damage, Speed,  Range, Movement, Number, Uses, Relaod Time, Prefab,             Bleed, Poison, SLow
 		attackType [0] = new Attack ("Starting Pistol", 0.5f,   150.0f, 30.0f,  0,          1,      6,  1.0f,       projectiles [0],    false, false, false);
 		attackType [1] = new Attack ("Web",             30.0f,   25.0f,  15.0f,  1,          1,      8,    2f,       projectiles [1],    false, true, true);
-		attackType [2] = new Attack ("Crab Claw",       50.0f,   20.0f,  50.0f,  0,          1,      2,    3f,       projectiles [2],    false, false, false);
+		attackType [2] = new Attack ("Crab Claw",       10.0f,   20.0f,  50.0f,  0,          1,      2,    3f,       projectiles [2],    false, false, false);
         attackType [3] = new Attack ("Wing",             1.5f,   25.0f,  10.0f,  1,          3,      2,  2.0f,       projectiles[0],     true, false, true);
     }
 }

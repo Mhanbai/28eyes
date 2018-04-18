b42a4565d7851c2a90794acdf295d031f0492ba7
BENJAMIN SAINT <1602915@uad.ac.uk>
2018-04-18 11:27:40 +0100

    New Ultimate Weapon

--- Assets/Scripts/ItemList.cs
+++ Assets/Scripts/ItemList.cs
@@ -21,9 +21,10 @@ public class ItemList : MonoBehaviour {
 
     //Item lists for each level
     [System.NonSerialized] public Item[] levelOneItems = new Item[9];
-    [System.NonSerialized] public Item[] levelTwoItems = new Item[3];
+    [System.NonSerialized] public Item[] levelTwoItems = new Item[1];
+    [System.NonSerialized] public Item[] levelThreeItems = new Item[1];
 
-	void Start() {
+    void Start() {
 		// 	                        Description,                                                                                            Objective?, Picture, Category, Health Change, Speed Change, Ammo, Reload, Range, Attack Type, Body Part Sprite) {
 		levelOneItems[0] = new Item("Equips the body of a crab, raising health by 30 but reducing speed by 20",                             false,      1,          1,      30,              -0.2f,      0,      0.0f,   0.0f,   -1,         0);
 		levelOneItems[1] = new Item("Equips the body of an eagle, increasing speed by 20% but lowering health by 20",                       false,      1,          1,      -20,             +0.2f,      0,      0.0f,   0.0f,   -1,         1);
@@ -35,9 +36,7 @@ public class ItemList : MonoBehaviour {
         levelOneItems[7] = new Item("Equips the legs of an eagle, raising your speed by 20% and your health by 10",                         false,      1,          3,      +10,             +0.2f,     0,      0.0f,   0.0f,   -1,         1);
         levelOneItems[8] = new Item("Equips the legs of a spider, Raising your speed by 30% but lowering your health by 10",                false,      1,          3,      -10,             +0.3f,     0,      0.0f,   0.0f,   -1,         0);
 
-        levelTwoItems[0] = new Item("Raises health by 30 but lowers speed by 10%", false, 1, 0, 30, 0, 0, 0.0f, 0.0f, -1, 0);
-		levelTwoItems[1] = new Item("Increases speed by 20% but lowers ammo by 10%", false, 1, 3, 0, 0, 0, 0, 0.0f, -1, 0);
-		levelTwoItems[2] = new Item("Lowers health by 5 and speed by 5%, equips a slowing web attack", false, 1, 2, -5, 0, 0, 0.0f, 0.0f, 1, 0);
+        levelTwoItems[0] = new Item("Equips the Ultimate Weapon, use at your own risk!",                                                    false,      1,          2,      0,                  0,      0,      0.0f,   0.0f,    4,         0);
 	}
 
 	public Item[] LevelItems(int listNo) {
